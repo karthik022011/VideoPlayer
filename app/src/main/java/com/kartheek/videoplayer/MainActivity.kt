7068229d2e484e3936a08df6a1c8c16a9a58ccc0
karthik1220 <karthikgithub1102@gmail.com>
2023-05-26 22:08:40 +0530

    initial commit

--- app/src/main/java/com/kartheek/videoplayer/MainActivity.kt
+++ app/src/main/java/com/kartheek/videoplayer/MainActivity.kt
@@ -2,10 +2,31 @@ package com.kartheek.videoplayer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.media3.common.MediaItem
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.ui.PlayerView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var playerView:PlayerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        playerView = findViewById(R.id.player_view)
+
+        val player = ExoPlayer.Builder(this).build()
+
+        playerView.player = player
+
+        // Build the media item.
+        val mediaItem = MediaItem.Builder()
+            .setUri("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4")
+            .build()
+
+        // Set the media item to be played.
+        player.setMediaItem(mediaItem)
+        // Prepare the player.
+        player.prepare()
+        // Start the playback.
+        player.play()
     }
 }
